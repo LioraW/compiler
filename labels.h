0b43bffaf8edd80ec92e87433c5cd4197280098c
LioraW <lcwdmg@umsystem.edu>
2022-05-02 10:18:55 -0500

    adding labels:

---
+++ labels.h
@@ -0,0 +1,29 @@
+#ifndef LABELS_H
+#define LABELS_H
+
+enum Label {
+    PROGRAM_LBL,
+    BLOCK_LBL,
+    VARS_LBL,
+    EXPR_LBL,
+    N_LBL,
+    X_LBL,
+    A_LBL,
+    M_LBL,
+    R_LBL,
+    STATS_LBL,
+    MSTATS_LBL,
+    STAT_LBL,
+    IN_LBL,
+    OUT_LBL,
+    IF_LBL,
+    LOOP1_LBL,
+    LOOP2_LBL,
+    ASSIGN_LBL,
+    RO_LBL,
+    LABEL_LBL,
+    GOTO_LBL,
+
+};
+
+#endif
